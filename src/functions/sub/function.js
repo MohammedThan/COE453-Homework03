f51f447ebd0835eb7f7cd2a8c2ecf1c7e695ae46
MohammedThan <114520655+MohammedThan@users.noreply.github.com>
2024-03-13 20:26:41 +0300

    Update function parameters from query to body

--- src/functions/sub/function.js
+++ src/functions/sub/function.js
@@ -1,5 +1,5 @@
 exports.sub = (req, res) => {
-  const { X, Y } = req.query;
+  const { X, Y } = req.body;
   const result = Number(X) - Number(Y);
   res.status(200).json({ X, Y, Result: result });
 };
