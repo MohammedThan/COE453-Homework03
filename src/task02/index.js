f38001b780f906682f422fd90477f8a50b2cce75
MohammedThan <114520655+MohammedThan@users.noreply.github.com>
2024-03-14 01:50:14 +0300

    implement task02 apiGateway

--- src/task02/index.js
+++ src/task02/index.js
@@ -20,3 +20,15 @@ functionNames.forEach((functionName) => {
   console.log("pushCommand: ", pushCommand);
   console.log("deployCommand: ", deployCommand, "\n");
 });
+
+const CONFIG_NAME = "task02";
+const BELONG_NAME = "task02";
+const YAML_FILE_PATH = "src/task02/apiGateway/task02.yaml";
+const PROJECT_ID = "mohammed-coe453";
+const GATEWAY_NAME = "task02";
+const REGION = "us-central1";
+const apiCommand = `gcloud api-gateway api-configs create ${CONFIG_NAME} --api=${BELONG_NAME} --openapi-spec=${YAML_FILE_PATH} --project=${PROJECT_ID}`;
+const gatewayCommand = `gcloud api-gateway gateways create ${GATEWAY_NAME} --api=${BELONG_NAME} --api-config=${CONFIG_NAME} --location=${REGION} --project=${PROJECT_ID}`;
+
+console.log(apiCommand, "\n");
+console.log(gatewayCommand, "\n");
